2d406b2cbfbf2aa2a9ddbdaf9bd588226ac5efb5
bahoerres <bahoerres@gmail.com>
2024-10-31 17:39:52 -0500

    Fix baseUrl configuration with leading slash

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "DIFFrnt",
+    baseUrl: "/DIFFrnt",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
